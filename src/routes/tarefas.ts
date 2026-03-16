57da36a8ac09a1c54f39856c9071c97388aaf654
gbgraciano-hue <gbgraciano@gmail.com>
2026-03-16 23:52:11 +0000

    aula 16/03/2026

--- src/routes/tarefas.ts
+++ src/routes/tarefas.ts
@@ -15,13 +15,54 @@ router.get("/", (req: Request, res: Response) =>{
 router.get("/:id", (req: Request, res: Response) => {
     const id = Number(req.params.id);
 
-    const tarefa = tarefas.find(t => t.id == id);
+    const tarefa = tarefas./z(t => t.id == id);
 
     if(!tarefa) {
         return res.status(404).json({erro: "Tarefa não encontrada"});
     }
 
     res.json(tarefa);
+});
+
+router.post("/", (req: Request, res: Response) => {
+    const {titulo} = req.body;
+
+    const novaTarefa: Tarefa = {
+        id: tarefas.length + 1,
+        titulo: titulo,
+        concluida: false
+    }
+
+
+    tarefas.push(novaTarefa);
+    res.status(201).json(novaTarefa);
+});
+
+router.put("/:id", (req: Request, res: Response) => {
+    const id = Number(req.params.id);
+
+    const tarefa = tarefas.find(t => t.id == id);
+
+    if(!tarefa) {
+        return res.status(404).json({erro : "Tarefa não encontrada"});
+    }
+
+    const {titulo, concluida} = req.body;
+
+    tarefa.titulo = titulo ?? tarefa.titulo,
+    tarefa.concluida = concluida ?? tarefa.concluida
+
+    res.json(tarefa);
+});
+
+router.delete("/:id", (req: Request, res: Response) => {
+    const id = Number(req.params.id);
+
+    tarefas = tarefas.filter(t => t.id !== id);
+
+    res.json({
+        mensagem: "Tarefa Removida"
+    });
 });
 
 export default router;
